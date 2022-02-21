22a58ef26388343b736aff56a172eab5a158ed49
João Campos <joaog.taborda.campos@gmail.com>
2022-02-21 11:55:09 -0300

    Adiciona endpoit para adição e listagem de fornecedor

--- api/rotas/index.js
+++ api/rotas/index.js
@@ -1,9 +1,32 @@
 const roteador = require('express').Router()
 const tabelaFornecedor = require('./tabelaFornecedor')
+const Fornecedor = require('./fornecedor')
 
-roteador.use('/', async (requisicao, resposta) => {
+roteador.get('/', async (requisicao, resposta) => {
   const resultados = await tabelaFornecedor.listar()
   resposta.send(JSON.stringify(resultados))
+})
+
+roteador.post('/', async (requisicao, resposta) => {
+  const dadosRecebidos = requisicao.body
+  const fornecedor = new Fornecedor(dadosRecebidos)
+  await fornecedor.criar()
+  resposta.send(JSON.stringify(fornecedor))
+})
+
+roteador.get('/:idFornecedor', async (requisicao, resposta) => {
+  try {
+    const id = requisicao.params.idFornecedor
+    const fornecedor = new Fornecedor({ id: id })
+    await fornecedor.carregar()
+    resposta.send(JSON.stringify(fornecedor))
+  } catch (erro) {
+    resposta.send(
+      JSON.stringify({
+        mensagem: erro.message
+      })
+    )
+  }
 })
 
 module.exports = roteador
