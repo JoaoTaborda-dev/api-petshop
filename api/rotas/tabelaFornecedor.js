70ba87716c6dcee987a0d6b5729a3e58678b0f8c
João Campos <joaog.taborda.campos@gmail.com>
2022-02-21 13:33:23 -0300

    Criação de endpoint para atualização de dados de um fornecedor

--- api/rotas/tabelaFornecedor.js
+++ api/rotas/tabelaFornecedor.js
@@ -20,5 +20,11 @@ module.exports = {
     }
 
     return encontrado
+  },
+
+  atualizar(id, dadosParaAtualizar) {
+    return Modelo.update(dadosParaAtualizar, {
+      where: { id: id }
+    })
   }
 }
